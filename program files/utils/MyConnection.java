78f0c90bfbe035ed3585d5cabd0195fc3e36ddd9
karthikeya-reddy011 <75920602+karthikeya-reddy011@users.noreply.github.com>
2022-08-09 21:32:08 +0530

    Add files via upload

---
+++ program files/utils/MyConnection.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Jyoti
+ */
+public class MyConnection {
+    public static Connection getConnection(){
+     
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/college_predictor", "root", "dbms");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return con;
+}
+}
